3470134340b062ac3b8076dc034323092e7ccbbf
OscarH1025 <oscarhurtado@Oscars-MacBook-Air.local>
2022-03-23 00:29:25 -0400

    loop

--- script.js
+++ script.js
@@ -19,12 +19,25 @@ var passwordCapital = confirm ("would you like to include uppercase letters");
 var passwordLowercase = confirm ("would you like to include lowercase letters?");
 var passwordNumeric = confirm ("would like to include numbers?");
 var passwordCapital === true || passwordLowercase === true || passwordSpecial === true) {
+ //if uppercase letter are selected, its added to the variable "passString"
   if (passwordCapital === true) {
     passString += capital
   }
 
-  //if lowercase letters are picked, its added to the variable 'passString'
+  //if lowercase letters are selected, its added to the variable 'passString'
+if (passwordLowercase === true) {
+  passString += lower
+
+  //if special characters are selected, its added to the variable "passString"
+  if (passwordSpecial === true) {
+    passString += special
+
+  }
 }
+
+// This statement alets if none of the variable were selected and goes back to the beginning of characterTyes function/
+else {
+  alert("Please choose at least one character type.")
 }
 
 // Get references to the #generate element
