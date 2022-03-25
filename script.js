958b3b4dc3b25034220151bc2fedcd9ba950d60d
OscarH1025 <oscarhurtado@Oscars-MacBook-Air.local>
2022-03-25 09:47:21 -0400

    Final

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Assignment code heres
 var passwordCapital = "";
 var passwordLowercase = "";
@@ -10,55 +9,73 @@ var numeric = "1234567890";
 var special = "!@#$%^&*()_+-=[]{},./?";
 var passString = "";
 var passResult = "";
-//textArea display the password on screen//
+//textArea displays the password on screen
+var textArea = document.getElementById("password");
 
-var textArea = document.getElementById("password")
+function characterTypes() {
+  var passwordCapital = confirm("Do you want to include Capital letters ");
+  var passwordLowercase = confirm("Do you want to include Lowercase letters?");
+  var passwordNumeric = confirm("Do you want to include Numbers?");
+  var passwordSpecial = confirm("Do you want to include Special characters?");
+  if (passwordCapital === true || passwordLowercase === true || passwordNumeric === true || passwordSpecial === true) {
+    //if uppercase letters are selected its added to the variable "passString"
+    if (passwordCapital === true) {
+      passString += capital
+    }
+    //if lowercase letters are selected, its added to the variable "passString"
+    if (passwordLowercase === true) {
+      passString += lower
+    }
+    //if numbers are selected, its added to the variable "passString"
+    if (passwordNumeric === true) {
+      passString += numeric
+    }
+    //if special characters are selected, its added to the variable "passString"
+    if (passwordSpecial === true) {
+      passString += special
+    }
 
-function characterTypes () {
-var passwordCapital = confirm ("would you like to include uppercase letters");
-var passwordLowercase = confirm ("would you like to include lowercase letters?");
-var passwordNumeric = confirm ("would like to include numbers?");
-var passwordCapital === true || passwordLowercase === true || passwordSpecial === true) {
- //if uppercase letter are selected, its added to the variable "passString"
-  if (passwordCapital === true) {
-    passString += capital
   }
-
-  //if lowercase letters are selected, its added to the variable 'passString'
-if (passwordLowercase === true) {
-  passString += lower
-
-  //if special characters are selected, its added to the variable "passString"
-  if (passwordSpecial === true) {
-    passString += special
-
-  }
+  //This statement else if none of the variable were selected and goes back to the beginning of characterTyes function/
+  else {
+    alert("You have to at least chose one element.")
+    characterTypes()
+  };
 }
 
-// This statement else if none of the variable were selected and goes back to the beginning of characterTyes function/
-else {
-  alert("Please choose at least one character type.")
-}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var text = '';
-
-  // Function questions allows for the prompt to loop back if a value less than 8 or greater than 128 was chosen
+  // function questions allows for the prompt to loop back if a value less than 8 or greater than 128 was chosen
   function questions() {
     var passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 characters to 128 characters.");
     if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128) {
       characterTypes()
+      //
+      for (let i = 0; i < passwordLength; i++) {
+        let x = passString[Math.floor(Math.random() * passString.length)]
+        passResult += x;
+      }
+      console.log(passResult);
+      console.log(textArea);
+      text = document.createTextNode(passResult);
+      //removes old password in the instance "generate password" was clicked after the first was made
+      textArea.innerHTML = '';
+      textArea.appendChild(text);
+      passResult = '';
 
-   for (let i = 0; i < passwordLength; i++)
-      
-  var passwordText = document.querySelector("#password");
+    };
 
-  passwordText.value = password;
 
+  }
+  questions()
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// writePassword now connected with the generate password button
+generateBtn.addEventListener("click", () => {
+  writePassword();
+});
